0a02f2015405b43309e4ac1bd63736489e636059
Josh Espinoza <joshespi@gmail.com>
2024-10-15 10:33:12 -0600

    prettier formatting

--- wp-content/themes/amelia-child/header.php
+++ wp-content/themes/amelia-child/header.php
@@ -1,11 +1,15 @@
 <!DOCTYPE html>
-<html <?php language_attributes();?>>
+<html <?php language_attributes(); ?>>
+
 	<head>
 		<!-- Google tag (gtag.js) -->
 		<script async src="https://www.googletagmanager.com/gtag/js?id=G-0JSNW78RE0"></script>
 		<script>
 		  window.dataLayer = window.dataLayer || [];
-		  function gtag(){dataLayer.push(arguments);}
+
+		function gtag() {
+			dataLayer.push(arguments);
+		}
 		  gtag('js', new Date());
 		
 		  gtag('config', 'G-0JSNW78RE0');
@@ -13,33 +17,36 @@
 			
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		<title><?php if (is_home() ) {?>News | <?php } ?><?php if (is_page() ) {the_title();?> | <?php } ?><?php if ( is_single() ) {the_title(); ?> | <?php } ?><?php bloginfo('name'); ?></title>
+	<title><?php if (is_home()) { ?>News | <?php } ?><?php if (is_page()) {
+															the_title(); ?> | <?php } ?><?php if (is_single()) {
+																							the_title(); ?> | <?php } ?><?php bloginfo('name'); ?></title>
 		<link href="https://fonts.googleapis.com/css?family=Roboto:700,900" rel="stylesheet">
-		<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri() .'/assets/slick/slick.css' ?>"/>
-		<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri() .'/assets/slick/slick-theme.css' ?>"/>
+	<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri() . '/assets/slick/slick.css' ?>" />
+	<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri() . '/assets/slick/slick-theme.css' ?>" />
 		<link href="https://customer.cludo.com/css/templates/v1.1/essentials/cludo-search.min.css" type="text/css" rel="stylesheet">
 		<?php wp_head(); ?>
 	</head>
+
 	<body>
 		<a class="skip-to-menu" href="#mainMenu">Skip to main menu</a>
 		<a class="skip-to-content" href="#contentArea">Skip to main content</a>
 		<header id="pcsdBranding">
-			<img src="<?php echo get_template_directory_uri() .'/assets/icons/pcsd-logo-website-header-branding.png' ?>" alt="Provo City School District Logo" />
+		<img src="<?php echo get_template_directory_uri() . '/assets/icons/pcsd-logo-website-header-branding.png' ?>" alt="Provo City School District Logo" />
 			<h1><a href="https://provo.edu">Provo City School District</a></h1>
 		</header>
 		<header id="mainHeader">
-			<a href="<?php bloginfo( 'siteurl' ); ?>"><img src="<?php echo get_stylesheet_directory_uri().'/assets/images/header-logo.png'; ?>" alt="Elem" id="siteLogo" />
+		<a href="<?php bloginfo('siteurl'); ?>"><img src="<?php echo get_stylesheet_directory_uri() . '/assets/images/header-logo.png'; ?>" alt="Elem" id="siteLogo" />
 			<h1>Amelia Earhart Elementary School</h1>
 			</a>
 				<aside>
 					<ul class="sociallinks">
-						<li><a href="http://www.instagram.com/AmeliaProvo/"><img src="<?php echo get_template_directory_uri() .'/assets/icons/socialmedia-instagram-white.svg' ?>" alt="Link to Instagram" /></span>
+				<li><a href="http://www.instagram.com/AmeliaProvo/"><img src="<?php echo get_template_directory_uri() . '/assets/icons/socialmedia-instagram-white.svg' ?>" alt="Link to Instagram" /></span>
 							</a>
 						</li>
-						<li><a href="http://twitter.com/ameliaPCSD"><img src="<?php echo get_template_directory_uri() .'/assets/icons/socialmedia-twitter-white.svg' ?>" alt="Link to Twitter" /></span>
+				<li><a href="http://twitter.com/ameliaPCSD"><img src="<?php echo get_template_directory_uri() . '/assets/icons/socialmedia-twitter-white.svg' ?>" alt="Link to Twitter" /></span>
 							</a>
 						</li>
-						<li><a href="https://www.facebook.com/ameliaearhartprovo"><img src="<?php echo get_template_directory_uri() .'/assets/icons/socialmedia-facebook-white.svg' ?>" alt="Link to Facebook" /></span>
+				<li><a href="https://www.facebook.com/ameliaearhartprovo"><img src="<?php echo get_template_directory_uri() . '/assets/icons/socialmedia-facebook-white.svg' ?>" alt="Link to Facebook" /></span>
 							</a>
 						</li>
 					</ul>
@@ -60,7 +67,6 @@
 		<nav id="mainMenu" role="navigation">
 			<input type="checkbox" id="reveal-menu" role="button">
 			<label class="mobileMenu" for="reveal-menu" onclick><img src="<?php echo get_template_directory_uri() . '/assets/icons/menu.svg'; ?>" alt="" />Menu</label>
-			<?php get_template_part( 'mainmenu'); ?>
+			<?php get_template_part('mainmenu'); ?>
 		</nav>
 		</header>
-		
